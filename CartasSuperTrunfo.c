c4b8a98dc27230b2e25e73e6463c7cd1d4eeafc9
EdilsonGondim <edilsonisaias4915@gmail.com>
2025-02-23 22:42:26 -0300

    atualizando desafio da faculdade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,12 @@ int main() {
 
     //Iniciando o código.
     char estado;
-    char codigo_da_carta[20]; //cdc = código da cidade.
-    char nome_da_cidade[20]; // ndc = nome da cidade;  
+    char codigo_da_carta[20]; 
+    char nome_da_cidade[20];  
     int populacao;
-    int area_em_km; // ak = Área em km².
-    int pib;
-    int numero_ponto_turistico; //npt = número de ponto turístico.
+    float area_em_km;
+    float pib;
+    int numero_ponto_turistico;
     
     //printf("=-=-=-=-=- CARTAS SUPER TRUNFO -=-=-=-=-=\n");
     printf("CADASTRE A CARTA 1: \n");
@@ -20,22 +20,21 @@ int main() {
     printf("CODIGO DA CARTA:(A letra do estado seguida de um numero de 01 a 04): \n");
     scanf("%s", &codigo_da_carta);
 
-    printf("NOME DA CIDADE: \n");
+    printf("NOME DA CIDADE:(sem espaco e sem caracteres especiais)  \n"); // Com espaco da erro no programa
     scanf("%s", &nome_da_cidade);
 
     printf("POPULACAO: \n");
     scanf("%d", &populacao);  
-
-    printf("AREA EM KM2: \n");
-    scanf("%d", &area_em_km);
+    printf("AREA EM KM²: \n");
+    scanf("%f Km2", &area_em_km);
 
     printf("PIB: \n");
-    scanf("%d", &pib);
+    scanf("%f Bilhoes de reais.", &pib);
 
     printf("NUMERO DE PONTOS TURISTICOS: \n");
     scanf("%d", &numero_ponto_turistico);
     
-    printf("=-=-=-= CARTA 1 CADASTRADA COM SUCESSO =-=-=-=\n");
+    printf("\n======CARTA 1 CADASTRADA COM SUCESSO======\n");
 
     printf("ESTADO: %c\nCODIGO DA CARTA: %s\nNOME DA CIDADE: %s\nPOPULACAO: %d\nAREA EM KM2: %d\nPIB: %d\nNUMERO DE PONTOS TURISTICOS: %d\n\n", estado, codigo_da_carta, nome_da_cidade, populacao, area_em_km, pib, numero_ponto_turistico);
 
