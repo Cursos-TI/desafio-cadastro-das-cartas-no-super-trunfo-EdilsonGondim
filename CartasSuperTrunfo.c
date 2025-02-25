a9e28492de6466205633ed05ee1efdd67bb71d13
EdilsonGondim <edilsonisaias4915@gmail.com>
2025-02-25 01:43:19 -0300

    atualização do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,7 @@ int main() {
     char codigo_da_carta[20]; 
     char nome_da_cidade[20];  
     int populacao;
-    float area_em_km;
-    float pib;
+    float area_em_km, pib, densidade_populacional, pib_per_capita;
     int numero_ponto_turistico;
     
     //printf("=-=-=-=-=- CARTAS SUPER TRUNFO -=-=-=-=-=\n");
@@ -35,9 +34,19 @@ int main() {
     printf("NÚMERO DE PONTOS TURÍSTICOS: ");
     scanf("%d", &numero_ponto_turistico);
     
-    printf("\n======CARTA 1 CADASTRADA COM SUCESSO======\n");
+    densidade_populacional = populacao / area_em_km;
 
-    printf("ESTADO: %c\nCÓDIGO DA CARTA: %s\nNOME DA CIDADE: %s\nPOPULAÇÃO: %d\nÁREA EM KM²: %.2f KM²\nPIB: %.2f Bilhões de reais\nNÚMERO DE PONTOS TURISTICOS: %d\n\n", estado, codigo_da_carta, nome_da_cidade, populacao, area_em_km, pib, numero_ponto_turistico);
+    pib_per_capita = pib / populacao;
+
+    printf("\n\n======CARTA 1 CADASTRADA COM SUCESS======\n");
+
+    printf("ESTADO: %c\nCÓDIGO DA CARTA: %s\nNOME DA CIDADE: %s\nPOPULAÇÃO: %d\nÁREA EM KM²: %.2f KM²\nPIB: %.2f Bilhões de reais\nNÚMERO DE PONTOS TURISTICOS: %d\nDENSIDADE POPULACIONAL %.2f hab/km²\nPIB PER CAPITA %.2f reais", estado, codigo_da_carta, nome_da_cidade, populacao, area_em_km, pib, numero_ponto_turistico, densidade_populacional, pib_per_capita);
 
     return 0; 
 }
+/*fortaleza
+pib: 12.874.175.00
+população: 2.574.412
+km²: 312.353 km²
+nº pontos turisticos: 150
+*/
